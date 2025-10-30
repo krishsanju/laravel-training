e9fd9cd1f843f18833b92340fb168cde21adb335
keerthana-sss <keerthana.saiKopanathi@sailssoftware.com>
2025-10-30 08:20:30 +0530

    SSO login..

---
+++ Keerthana/Laravel/New Course/app/Http/Controllers/GoogleController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try{
+            $user = Socialite::driver('google')->user();
+
+            $findUser = User::where('google_id', $user->getId())->first();
+
+            if($findUser){
+                Auth::login($findUser);
+                return redirect()->intended('/LOGGEDIN');
+            }else{
+                $newUser = User::create([
+                    'name' => $user->getName(),
+                    'email' => $user->getEmail(),
+                    'google_id' => $user->getId(),
+                    'password' => bcrypt('password'),  //use ledhu anaru but petali antunaru
+                ]);
+
+                Auth::login($newUser);
+                return redirect()->intended('/LOGGEDIN');
+            }
+            
+        }catch(\Exception $e){
+            return redirect('/login')->with('error', "Google login failed");
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
--- Keerthana/Laravel/New Course/routes/web.php
+++ Keerthana/Laravel/New Course/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/LOGGEDIN', function(){
+    return "LOGGEDIN";
+});
+
+Route::get('/auth/google', [GoogleController::class, 'redirectToGoogle'])->name('login.google');
+Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+Route::get('logout', [GoogleController::class, 'logout'])->name('logout');
