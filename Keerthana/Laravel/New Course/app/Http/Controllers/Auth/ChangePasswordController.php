c9fe8493614fbb93bb36fb41dfc6816f2bc78e10
keerthana-sss <keerthana.saiKopanathi@sailssoftware.com>
2025-11-03 22:57:26 +0530

    set up , change password code , add tokens (register, login)

---
+++ Keerthana/Laravel/New Course/app/Http/Controllers/Auth/ChangePasswordController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Response;
+use App\Http\Responses\ApiResponse;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Auth\ChangePasswordRequest;
+
+class ChangePasswordController extends Controller
+{
+    public function __construct(private ApiResponse $apiResponse) {}
+    public function update(ChangePasswordRequest $request)
+    {
+        $user = $request->user();
+
+        if(! Hash::check($request->currentPassword , $user->password))
+        {
+            return $this->apiResponse->setMessage("Current password is incorrect")
+                                ->response(Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        User::updatePassword($user, $request->password);
+        return $this->apiResponse->setMessage("Password updated successfully")
+                            ->response(Response::HTTP_OK);
+    }
+}
---
+++ Keerthana/Laravel/New Course/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+
+        // event(new Registered($user));
+
+        // Auth::login($user);
+
+        // return redirect(route('dashboard', absolute: false));
+    }
+}
